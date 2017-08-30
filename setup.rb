b691caeea009e30d696f1ffcc572acd0d0bef603
KriCse <you@example.com>
2017-08-30 15:57:53 +0200

    root Test

--- setup.rb
+++ setup.rb
@@ -4,8 +4,21 @@ def init()
   json_file = File.read "config.json"
   options = JSON.parse(json_file)
   @install_command = options['install_command']
+  @dir = options['dotfiles_location']
+end
+
+def setup_zsh()
+  install_package("zsh")
+end
+
+def install_package(package_name)
+  cmd = "#{@install_command} #{package_name}"
+  puts "Installing #{package_name}"
+  t = %x{ #{cmd} }
 end
 
 init()
-puts @install_command
+puts @dir
+setup_zsh
+
 
