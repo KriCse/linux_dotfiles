b2d934deedd43aa03c08031801c4fbe7d8d9d441
KriCse <kristofcserpes@gmail.com>
2017-08-30 14:19:02 +0200

    test

--- setup.rb
+++ setup.rb
@@ -1,1 +1,11 @@
-puts "test"
+require 'json'
+
+def init()
+  json_file = File.read "config.json"
+  options = JSON.parse(json_file)
+  @install_command = options['install_command']
+end
+
+init()
+puts @install_command
+
